cae1241c1d6e2d8165b180dd2a6425c443b7c43a
Sasi2294 <montella.salvatore@live.it>
2023-11-27 10:33:32 +0100

    repo, folders and start working

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+
+function bookService(event) {
+    event.preventDefault();
+
+    let hoursSelected = document.getElementById("hours").value;
+    hoursSelected = parseFloat(hoursSelected);
+    //console.log(hoursSelected)
+    let backEndDev = document.getElementById("backEnd").innerText;
+    //console.log(backEndDev)
+}
